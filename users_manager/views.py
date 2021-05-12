4bc4941aea9a0af26727bae10881a0571228285a
Gil <giltenoriov@gmail.com>
2021-05-12 16:44:41 -0500

    Integrado el clasificador

--- users_manager/views.py
+++ users_manager/views.py
@@ -8,9 +8,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from users_manager.forms import FormArchivos, ProfileForm
-import joblib
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView, CreateView
+from django.views.generic import  CreateView
+"""Instalar las siguientes librerias"""
+import joblib
+import json
 
 # Create your views here.
 def index(request):
@@ -67,7 +69,6 @@ def entrada(request):
        form = FormArchivos(request.POST, request.FILES)
        messages = 'Error: '
        if form.is_valid():
-            print(id)
             form.save()  
             return redirect('listaArchivos')
        else:
@@ -79,19 +80,100 @@ def entrada(request):
 @login_required()
 def file_list(request):
     id_user = request.session['_auth_user_id']
-    #us = CustomUser.objects.filter(user = id_user)
-    #print(us)
-    #files = Archivo.objects.all()
+    
     files = Archivo.objects.filter(user__user= id_user)
     contexto = {'files': files}
 
     return render(request, 'listaFiles.html',contexto)
 
 def file_classifier(request,id_file):
-    file = Archivo.objects.get(id = id_file)
+    class mensaje:
+        
+        def __init__(self,message,user, predict = None):
+            self.message = message
+            self.user = user
+            self.predict = predict
+
+    def predecir(texto):
+        """
+        Esta función carga el modelo entrenado y el vector, el modelo tiene una exactitud del 79%
+        Recibe un String
+        Retorna 0 si el texto lo clasifica como no ofensivo y 1 si lo clasifica como ofensivo
+        """
+        modelo = joblib.load('users_manager/research/modeloEntrenado.pkl')
+        vector = joblib.load('users_manager/research/vector.pkl')
+
+        matriz = vector.transform([texto])
+        return modelo.predict(matriz)
     
+    file = Archivo.objects.get(id = id_file).file
+    file_name = Archivo.objects.get(id = id_file).name
     
-    return render(request, "classifier.html", {'id': id_file})
+    lista =list()
+    name = set()
+    agressive = 0
+    noAgressive = 0
+
+    with file.open(mode='rb') as contenido:
+        datos = json.load(contenido)
+
+        for m in datos['message']:
+            objetoMessage =  mensaje(m['text'],m['name'])
+            message = m['text']
+            pre =predecir(message)
+            objetoMessage.predict = pre
+            lista.append(objetoMessage)
+            name.add(m['name'])
+            if pre == 1:
+                agressive += 1
+            else:
+                noAgressive += 1
+    
+    user1_NoAgressive = 0
+    user1_Agressive = 0
+    user2_NoAgressive = 0
+    user2_Agressive = 0
+    users = list(name)
+
+    for u in lista:
+        if u.user == users[0]:
+            if u.predict == 0:
+                user1_NoAgressive += 1
+            else:
+                user1_Agressive += 1
+        else:
+            if u.predict == 0:
+                user2_NoAgressive += 1
+            else:
+                user2_Agressive += 1 
+    
+
+    labelsGeneral = []
+    dataGeneral = []
+    labelsGeneral.append('Agresivos')
+    labelsGeneral.append('No Agresivos')
+    dataGeneral.append(agressive)
+    dataGeneral.append(noAgressive)
+
+    labelsUser1 = []
+    dataUser1 = []
+    labelsUser1.append('Agresivos')
+    labelsUser1.append('No Agresivos')
+    dataUser1.append(user1_Agressive)
+    dataUser1.append(user1_NoAgressive)
+
+    labelsUser2 = []
+    dataUser2 = []
+    labelsUser2.append('Agresivos')
+    labelsUser2.append('No Agresivos')
+    dataUser2.append(user2_Agressive)
+    dataUser2.append(user2_NoAgressive)            
+        
+    context = { 'lista': lista, 'labelsG': labelsGeneral, 'dataG':dataGeneral,'file_name':file_name,
+                'labelsU1': labelsUser1, 'dataU1': dataUser1, 'labelsU2': labelsUser2, 'dataU2': dataUser2,
+                'users': users}
+    
+    return render(request, "classifier.html", context)
 
 def file_delete(request,id_file):
     f = Archivo.objects.get(id=id_file)
